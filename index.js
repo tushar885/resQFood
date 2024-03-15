d7dcc06853d2c828748e5a0e6ff22a9793e5baf7
tushar885 <tushar8104@gmail.com>
2024-03-16 00:07:03 +0530

    removed issue of parcel-cache and dist in the .gitignore file

---
+++ index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import AppLayout from "./src/AppLayout";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
